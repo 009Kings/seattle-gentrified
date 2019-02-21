5258bfe2fbef89d2bfcd2725c15513a07a17e707
009kings <009kings@gmail.com>
2019-02-21 15:18:22 -0800

    less ugly map now centered on Seattle

--- src/components/heatmap/Heatmap.js
+++ src/components/heatmap/Heatmap.js
@@ -1,3 +1,4 @@
+import './heatmap.css'
 import React, { Component } from 'react';
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
@@ -5,22 +6,31 @@ const Map = ReactMapboxGl({
   accessToken: "pk.eyJ1IjoiYm9kaGkta2luZyIsImEiOiJjanFta3hod3cxYnBpNDNtMDAxd2N1cXB4In0.orNcNCcYg6Kmat20sEZ8wA"
 });
 
+const mapStyles = {
+  height: "50vh",
+  width: "50vw",
+  margin: "0 auto"
+}
+
 class Heatmap extends Component {
   render() {
     return (
-      <Map
-        style="mapbox://styles/mapbox/streets-v9"
-        containerStyle={{
-          height: "100vh",
-          width: "100vw"
-        }}>
-          <Layer
-            type="symbol"
-            id="marker"
-            layout={{ "icon-image": "marker-15" }}>
-            <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
-          </Layer>
-      </Map>
+      <div id="map-container">
+        <Map
+          style="mapbox://styles/mapbox/streets-v9?optimize=true"
+          container="map-container"
+          center={{lon: -122.335167, lat:47.608013 }}
+          zoom={[10]}
+          containerStyle={mapStyles} // Style of the map container
+          >
+            <Layer
+              type="symbol"
+              id="marker"
+              layout={{ "icon-image": "marker-15" }}>
+              <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+            </Layer>
+        </Map>
+      </div>
     )
   }
 }
