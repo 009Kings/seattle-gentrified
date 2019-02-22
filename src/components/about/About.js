9c80fdf1ec00f1665fa0f30ac4bb890fed4b9c17
009kings <009kings@gmail.com>
2019-02-22 14:47:17 -0800

    About section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,9 +5,9 @@ const About = () => {
     <div className="aboutContainer">
 	    <a name="About"></a>
 	    <h2>About this project</h2>
-	    <p><strong>Project intention.</strong> Lorem Ipsum ha estat el text estàndard de la indústria des de l'any 1500, quan un impressor desconegut va fer servir una galerada de text i la va mesclar per crear un llibre de mostres tipogràfiques. No només ha sobreviscut cinc segles, sinó que ha fet el salt cap a la creació de tipus de lletra electrònics, romanent essencialment sense canvis.</p>
-	    <p><strong>Team.</strong> The team that put this project together includes Sam Chakerian (data science), Victoria Baik and Serey Rachana Meng (UX design), and Sarah King and Tamis Nordling (web development).</p>
-
+	    <p><strong>Project intention.</strong> Many people in Seattle have heard the word Gentrification, but most of them are not informed what Gentrification is and how fast it is happening. Our aim is to help inform people through data visuals and information.</p>
+      <p><strong>Data Sources.</strong></p>
+	    <p><strong>Team.</strong> The team that put this project together includes Sam Chakerian (Data Science), Victoria Baik and Serey Rachana Meng (UX design), and Sarah King and Tamis Nordling (Software Engineering).</p>
     </div>
   )
 }
