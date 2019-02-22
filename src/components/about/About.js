45cfefbed2d6d218ec1c6053e51a3265ca954fa8
tam-en <tamis@leftelbow.com>
2019-02-22 10:30:09 -0800

    about, addtnl resources, fixed graph link

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,3 +1,15 @@
 import React, { Component } from 'react';
 
+const About = () => {
+  return (
+    <div className="aboutContainer">
+	    <a name="About"></a>
+	    <h1>About this project</h1>
+	    <p><strong>Project intention.</strong> Lorem Ipsum ha estat el text estàndard de la indústria des de l'any 1500, quan un impressor desconegut va fer servir una galerada de text i la va mesclar per crear un llibre de mostres tipogràfiques. No només ha sobreviscut cinc segles, sinó que ha fet el salt cap a la creació de tipus de lletra electrònics, romanent essencialment sense canvis.</p>
+	    <p><strong>Team.</strong> The team that put this project together includes Sam Chakerian (data science), Victoria Baik and Serey Rachana Meng (UX design), and Sarah King and Tamis Nordling (web development).</p>
+
+    </div>
+  )
+}
+
 export default About;
--- src/components/additionalResources/AdditionalResources.js
+++ src/components/additionalResources/AdditionalResources.js
@@ -1,3 +1,28 @@
 import React, { Component } from 'react';
 
+
+const AdditionalResources = () => {
+  return (
+    <div className="resourcesContainer">
+	    <a name="Additional"></a>
+	    <h1>Additional Resources</h1>
+	    <p><strong>Seattle-specific gentrification & related information</strong></p>
+	    <ul>
+			<li><a href="http://www.governing.com/gov-data/seattle-gentrification-maps-demographic-data.html">Seattle Gentrification Maps and Data</a></li>
+			<li><a href="https://www.instagram.com/vanishingseattle/"><em>vanishingseattle,</em> an Instagram account that documents "disappearing/displaced businesses, homes, communities & cultures of Seattle." </a></li>
+			<li><a href="http://www.seattleforgrowth.org/tale-two-maps-mayor-councils-new-redline/">"Tale of Two Maps: Seattle Mayor and Council Draw New Redline," Seattle for Growth</a></li>
+			<li><a href="https://www.seattle.gov/sdci">Seattle Department of Construction and Inspections</a></li>
+			<li><a href="http://blogs.seattletimes.com/fyi-guy/2014/12/17/explore-this-language-diversity-map-of-king-county/">"Explore this: Language diversity map of King County," <em>The Seattle Times</em>, December 17, 2014</a></li>
+			<li><a href="https://www.seattle.gov/housing/renters/find-housing">City of Seattle "Find Affordable Housing" tool</a></li>
+	    </ul>
+	    <p><strong>Resources on gentrification and affordable housing</strong></p>
+	    <ul>
+			<li><a href="https://dsl.richmond.edu/panorama/redlining/#loc=4/36.71/-96.93&opacity=0.8"><em>Mapping Inequality: Redlining in New Deal America,</em> a project of the University of Richmond's Data Scholarship Lab</a></li>
+			<li><a href="https://www.theatlantic.com/politics/archive/2015/09/this-is-what-happens-after-a-neighborhood-gets-gentrified/432813/">"This Is What Happens After a Neighborhood Gets Gentrified", <em>The Atlantic</em>, September 16, 2015</a></li>
+	    </ul>	    
+    </div>
+  )
+}
+
+
 export default AdditionalResources;
